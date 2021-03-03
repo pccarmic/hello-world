95db674545e9608701bf8c5b91c9bf8d20a664d6
pccarmic <72418739+pccarmic@users.noreply.github.com>
2021-03-02 17:06:03 -0700

    Swap findFactorial() and reverseNumber() code
    Swapped the code in findFactorial() and reverseNumber().

--- GitDemo.java
+++ GitDemo.java
@@ -25,10 +25,8 @@ public class GitDemo {
     public double findFactorial(int num) {
         double factorialNumber = 0;
         
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
         }
         
         return factorialNumber;
@@ -42,10 +40,12 @@ public class GitDemo {
     public double reverseNumber(int num){
         double newNumber = 1;
         
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
+        while( num != 0 ) {
+            double temp = num % 10;
+            newNumber = newNumber * 10 + temp;
+            num /= 10;
         }
-        
+
         return newNumber;
     }
     
